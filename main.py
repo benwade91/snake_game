bf0a98138c858c02d8793a7cec512b9084d97118
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-13 08:45:20 -0700

    created gameboard

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from turtle import Screen, Turtle
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+screen.exitonclick()
